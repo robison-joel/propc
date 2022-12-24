18f2f0025712c60339d5293e0862b622b901b964
Róbison Joel Guimarães Simon <64526672+robison-joel@users.noreply.github.com>
2022-12-24 19:54:13 -0300

    Add files via upload

---
+++ PC/pro_pc_3.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+##### CRIANDO LOGS
+# Criando a pasta de logs.
+sudo mkdir logs/
+# Atribuindo permissões á pasta de logs.
+sudo chmod -R 777 logs/
+# Criando o arquivo de logs.
+VAR_LOG="logs/log_propc_$(date +%d-%m-%Y).log"
+
+##### INICIO DO SCRIPT #################################
+echo "                                                  " > "$VAR_LOG"
+echo "##################################################" | sudo tee -a "$VAR_LOG"
+echo "                                                  " | sudo tee -a "$VAR_LOG"
+echo "INÍCIO DO SCRIP                                   " | sudo tee -a "$VAR_LOG"
+echo "                                                  " | sudo tee -a "$VAR_LOG"
+echo "DATA E HORA: $(date +%d-%m-%Y_%H:%M)              " | sudo tee -a "$VAR_LOG"
+echo "                                                  " | sudo tee -a "$VAR_LOG"
+echo "##################################################" | sudo tee -a "$VAR_LOG"
+echo "                                                  " | sudo tee -a "$VAR_LOG"
+##### Instalando pacotes necessários.
+echo "##################################################" | sudo tee -a "$VAR_LOG"
+echo "Instalando pacotes necessários.                   " | sudo tee -a "$VAR_LOG"
+echo "                                                  "
+echo "Instalando pacotes necessários...aguarde!         "
+echo "                                                  " | sudo tee -a "$VAR_LOG"
+echo "### Instalando o figlet ##########################" | sudo tee -a "$VAR_LOG"
+sudo apt install figlet -y | sudo tee -a "$VAR_LOG"
+echo "##################################################" | sudo tee -a "$VAR_LOG"
+echo "                                                  " | sudo tee -a "$VAR_LOG"
+
+# INSTALANDO O GRAVADOR DE AUDIO
+
+figlet Gravador de voz  | sudo tee -a "$VAR_LOG"
+echo "      ######################################      " | sudo tee -a "$VAR_LOG"
+echo "   ############################################   " | sudo tee -a "$VAR_LOG"
+echo "##################################################" | sudo tee -a "$VAR_LOG"
+echo "#########                                #########" | sudo tee -a "$VAR_LOG"
+echo "######                                      ######" | sudo tee -a "$VAR_LOG"
+echo "###                                            ###" | sudo tee -a "$VAR_LOG"
+echo "###    DESEJA INSTALAR O GRAVADOR DE AUDIO?    ###" | sudo tee -a "$VAR_LOG"
+echo "###                                            ###" | sudo tee -a "$VAR_LOG"
+echo "###    1 - INSTALAR                            ###" | sudo tee -a "$VAR_LOG"
+echo "###                                            ###" | sudo tee -a "$VAR_LOG"
+echo "###    2 - PULAR a instalação e continuar      ###" | sudo tee -a "$VAR_LOG"
+echo "###                                            ###" | sudo tee -a "$VAR_LOG"
+echo "###    3 - SAIR                                ###" | sudo tee -a "$VAR_LOG"
+echo "###                                            ###" | sudo tee -a "$VAR_LOG"
+echo "######                                      ######" | sudo tee -a "$VAR_LOG"
+echo "#########                                #########" | sudo tee -a "$VAR_LOG"
+echo "##################################################" | sudo tee -a "$VAR_LOG"
+echo "   ############################################   " | sudo tee -a "$VAR_LOG"
+echo "      ######################################      " | sudo tee -a "$VAR_LOG"
+echo "                                                  " | sudo tee -a "$VAR_LOG"
+echo "##################################################" | sudo tee -a "$VAR_LOG"
+echo "##### Digite sua opção! ##########################" | sudo tee -a "$VAR_LOG"
+echo "                                                  " | sudo tee -a "$VAR_LOG"
+read -r "VAR_GRAVADOR"
+echo "                                                  " | sudo tee -a "$VAR_LOG"
+echo "Tu apertastes o numero: $VAR_GRAVADOR" | sudo tee -a "$VAR_LOG"
+echo "                                                  " | sudo tee -a "$VAR_LOG"
+echo "##################################################" | sudo tee -a "$VAR_LOG"
+#
+if [ "$VAR_GRAVADOR" = "3" ]; then
+
+    echo "Vocẽ escolheu não instalar o gravador de audio e sair! " | sudo tee -a "$VAR_LOG"
+    echo "                                                  " | sudo tee -a "$VAR_LOG"
+    echo "3...                                              " | sudo tee -a "$VAR_LOG"
+    sleep 1s  | sudo tee -a "$VAR_LOG"
+    echo "                                                  " | sudo tee -a "$VAR_LOG"
+    echo "2..                                               " | sudo tee -a "$VAR_LOG"
+    sleep 1s
+    echo "                                                  " | sudo tee -a "$VAR_LOG"
+    echo "1.                                                " | sudo tee -a "$VAR_LOG"
+    sleep 1s
+    echo "                                                  " | sudo tee -a "$VAR_LOG"
+    echo "Tchau!                                            " | sudo tee -a "$VAR_LOG"
+    echo "                                                  " | sudo tee -a "$VAR_LOG"
+    echo "##################################################" | sudo tee -a "$VAR_LOG"
+    echo "                                                  " | sudo tee -a "$VAR_LOG"
+    echo "By                                                " | sudo tee -a "$VAR_LOG"
+    echo "               ##################                 " | sudo tee -a "$VAR_LOG"
+    echo "             ######################               " | sudo tee -a "$VAR_LOG"
+    echo "              _\|/_MonkeyWise_\|/_                " | sudo tee -a "$VAR_LOG"
+    echo "             ######################               " | sudo tee -a "$VAR_LOG"
+    echo "               ##################                 " | sudo tee -a "$VAR_LOG"
+    echo "                                                  " | sudo tee -a "$VAR_LOG"
+    exit
+
+elif [ "$VAR_GRAVADOR" = 2 ]; then 
+    
+    echo "Vocẽ escolheu pular a instalação do gravador de audio! " | sudo tee -a "$VAR_LOG"
+    echo "                                                  " | sudo tee -a "$VAR_LOG"
+    echo "Continuando...                                    " | sudo tee -a "$VAR_LOG"
+    sleep 2s
+    
+elif [ "$VAR_GRAVADOR" = 1 ]; then
+
+    echo "                                                  " | sudo tee -a "$VAR_LOG"
+    echo "Instalou!!!                                       " | sudo tee -a "$VAR_LOG"
+    # sudo add-apt-repository ppa:audio-recorder/ppa -y
+    # sudo apt-get update 
+    # sudo apt-get install audio-recorder -y
+    echo "                                                  " | sudo tee -a "$VAR_LOG"
+
+fi
+#
+echo "##################################################" | sudo tee -a "$VAR_LOG"
+echo "                                                  " | sudo tee -a "$VAR_LOG"
+echo "By                                                " | sudo tee -a "$VAR_LOG"
+echo "               ##################                 " | sudo tee -a "$VAR_LOG"
+echo "             ######################               " | sudo tee -a "$VAR_LOG"
+echo "              _\|/_MonkeyWise_\|/_                " | sudo tee -a "$VAR_LOG"
+echo "             ######################               " | sudo tee -a "$VAR_LOG"
+echo "               ##################                 " | sudo tee -a "$VAR_LOG"
+echo "                                                  " | sudo tee -a "$VAR_LOG"
