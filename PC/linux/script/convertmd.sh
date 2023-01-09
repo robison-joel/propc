9c522b68351ba6d0feddc2c566d04998d6a40f44
Róbison Joel Guimarães Simon <64526672+robison-joel@users.noreply.github.com>
2023-01-09 01:11:26 -0300

    Delete PC directory

--- PC/linux/script/convertmd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Qual o arquivo: "
-read "qualarquivo"
-
-# pandoc -f markdown -t html logins.md -o logins.html
-
-pandoc -f markdown -t html $qualarquivo.md -o $qualarquivo.html
